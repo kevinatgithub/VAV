027d856f77023e0eb6fdcc09085bedf37d2acb2b
Jerome Franco <jerome.franco@delaware.pro>
2018-09-17 13:02:55 +0800

    FE: Removed sidedialog in dashboard

--- client/src/modules/dashboard/components/dashboard.js
+++ client/src/modules/dashboard/components/dashboard.js
@@ -1,41 +1,18 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import { Switch, Classes, H5 } from '@blueprintjs/core';
-import { MainContent, Div, Button } from '../../ui';
+import { Switch } from '@blueprintjs/core';
+import { MainContent, Div } from '../../ui';
 import DocumentTitle from '../../common/document-title/document-title';
-import SideDialog from '../../common/side-dialog/containers/side-dialog-container';
 
 class Dashboard extends Component {
-  state = {
-    dialogOpen: false,
-  }
-
-  handleDialogOpen = () => {
-    this.setState({ dialogOpen: true });
-  }
-
-  handleDialogClose = () => {
-    this.setState({ dialogOpen: false });
-  }
+  state = {};
 
   render() {
     return (
       <MainContent>
         <DocumentTitle pageTitle='Board' />
-        <Button onClick={this.handleDialogOpen} icon='refresh' intent='danger'>
-          Open Dialog
-        </Button>
+        <Div>Under construction...</Div>
         <Switch label='Fullscreen' defaultChecked={true} />
-        <SideDialog
-          icon='info-sign'
-          isOpen={this.state.dialogOpen}
-          onClose={this.handleDialogClose}
-          title='Sample Dialog'
-        >
-          <Div className={Classes.DIALOG_BODY}>
-            <H5>Dashboard</H5>
-          </Div>
-        </SideDialog>
       </MainContent>
     );
   }
