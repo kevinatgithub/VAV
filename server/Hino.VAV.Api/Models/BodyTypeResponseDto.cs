4dd66f4e21f69348c6e97d793982102dcfc676b0
DJMarcelo <azure.jmarcelo@gmail.com>
2018-10-14 19:12:25 +0800

    Body Type List

---
+++ server/Hino.VAV.Api/Models/BodyTypeResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hino.VAV.Api.Models.BaseResponse;
+
+namespace Hino.VAV.Api.Models
+{
+    public class BodyTypeResponseDto : IDtoRoot
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
